5645dc60cff23effe203882d4e80da954799862e
Vladimir Shvets <vladimir.shvets1206@gmail.com>
2017-11-07 20:47:36 +0200

    Add SocialAssociation model

--- accounts/models.py
+++ accounts/models.py
@@ -8,3 +8,12 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class SocialAssociation(models.Model):
+    social_id = models.CharField(max_length=25)
+    social_network = models.CharField(max_length=20)
+    user = models.ForeignKey(User, related_name='social_association')
+
+    def __str__(self):
+        return self.user.email
