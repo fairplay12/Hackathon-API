cdc0007f4c30c01c66b93b0e1a8def93325d1baf
Vladimir Shvets <vladimir.shvets1206@gmail.com>
2017-11-07 22:16:29 +0200

    Change settings

--- hackathon/settings/base.py
+++ hackathon/settings/base.py
@@ -49,6 +49,17 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hackathon',
+        'USER': 'hackathon',
+        'PASSWORD': 'hackathon123',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
+
 ROOT_URLCONF = 'hackathon.urls'
 
 TEMPLATES = [
--- hackathon/settings/dev.py
+++ hackathon/settings/dev.py
@@ -1,13 +1,5 @@
 from .base import *
+import dj_database_url
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'hackathon',
-        'USER': 'hackathon',
-        'PASSWORD': 'hackathon123',
-        'HOST': 'db',
-        'PORT': '5432',
-    }
-}
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
