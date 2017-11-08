140fe2c450c72806f65153adb5472ca78e5d5e3a
Vladimir Shvets <vladimir.shvets1206@gmail.com>
2017-11-08 20:01:27 +0200

    Add static urls

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
