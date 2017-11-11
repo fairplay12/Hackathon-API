ab0f481194c304d1eb6a0d958ff5b364cda43592
Vladimir Shvets <vladimir.shvets1206@gmail.com>
2017-11-11 13:37:41 +0200

    Create models for Sport

--- sport/models.py
+++ sport/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class SportCategory(models.Model):
+    name = models.CharField(
+        max_length=255,
+    )
+    short_description = models.TextField()
+
+    def __str__(self):
+        return str(self.name)
+
+
+class SportSection(models.Model):
+    category = models.ForeignKey(
+        SportCategory,
+        related_name='sport_sections',
+    )
+    name = models.CharField(
+        max_length=255
+    )
+    description = models.TextField()
+    max_ppl_in_section = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Achievement(models.Model):
+    user = models.ForeignKey(
+        'accounts.User',
+        related_name='achievements'
+    )
+    name = models.CharField(
+        max_length=255,
+    )
+
+    def __str__(self):
+        return str(self.user)
+
+
+class Championship(models.Model):
+    name = models.CharField(
+        max_length=255,
+    )
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return str(self.name)
