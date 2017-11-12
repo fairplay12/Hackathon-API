ecf0ea4dec583e9efdc4965edacb3102b1c883c2
Vladimir Shvets <vladimir.shvets1206@gmail.com>
2017-11-12 18:23:00 +0200

    Add schema for Time model

--- management/schema.py
+++ management/schema.py
@@ -1,13 +1,19 @@
 import graphene
 from graphene_django.types import DjangoObjectType
 
-from .models import Review
+from .models import Review, Time
 
 
 class ReviewType(DjangoObjectType):
 
     class Meta:
         model = Review
+
+
+class TimeType(DjangoObjectType):
+
+    class Meta:
+        model = Time
 
 
 class Query:
@@ -19,9 +25,16 @@ class Query:
         ReviewType,
         user_id=graphene.ID()
     )
+    trainings_time = graphene.List(
+        TimeType,
+        section_id=graphene.ID()
+    )
 
     def resolve_reviews_about_section(self, info, section_id):
         return Review.objects.filter(section_id=section_id)
 
     def resolve_reviews_by_user(self, info, user_id):
         return Review.objects.filter(user_id=user_id)
+
+    def resolve_trainings_time(self, info, section_id):
+        return Time.objects.filter(sport_section_id=section_id)
