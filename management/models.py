5f9c3c762a6dbcfed62d6fc78655b5319f54d08b
Vladimir Shvets <vladimir.shvets1206@gmail.com>
2017-11-12 16:03:04 +0200

    Create Reviews model

--- management/models.py
+++ management/models.py
@@ -11,3 +11,22 @@ class Schedule(models.Model):
 
     def __str__(self):
         return self.sport_section
+
+
+class Review(models.Model):
+    user = models.ForeignKey(
+        'accounts.User',
+        related_name='reviews'
+    )
+    section = models.ForeignKey(
+        'sport.SportSection',
+        related_name='reviews'
+    )
+    text = models.TextField()
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    score = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return int(self.user.id)
